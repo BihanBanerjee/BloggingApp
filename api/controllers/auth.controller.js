9fa109847993d86e744f48c5f897c2f0af52c0a9
Bihan Banerjee <banerjeebihan456@gmail.com>
2024-07-06 11:57:26 +0530

    add a middleware and a function to handle errors.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,16 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
-export const signup = async (req, res) => {
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req, res, next) => {
     //console.log(req.body);
     const { username, email, password } = req.body;
     if (!username || !email || !password || username === "" || email === "" || password === "") {
-        return res.status(400).json({ msg: "Not all fields have been entered." });
+        // return res.status(400).json({ msg: "Not all fields have been entered." });
+        next(errorHandler(400, "Not all fields have been entered."));
     }
     if (password.length < 6) {
-        return res
-            .status(400)
-            .json({ msg: "The password needs to be at least 6 characters long." });
+        // return res.status(400).json({ msg: "The password needs to be at least 6 characters long." });
+        next(errorHandler(400, "The password needs to be at least 6 characters long."));
     }
 
     // Check if the user already exists
@@ -25,10 +26,12 @@ export const signup = async (req, res) => {
     // Check if the email or username already exists
     if (existingUser) {
         if (existingUser.email === email) {
-            return res.status(400).json({ msg: "An account with this email already exists." });
+            //return res.status(400).json({ msg: "An account with this email already exists." });
+            return next(errorHandler(400, "An account with this email already exists."));
         }
         if (existingUser.username === username) {
-            return res.status(400).json({ msg: "An account with this username already exists." });
+            //return res.status(400).json({ msg: "An account with this username already exists." });
+            return next(errorHandler(400, "An account with this username already exists."));
         }
     }
 
@@ -41,6 +44,11 @@ export const signup = async (req, res) => {
         email,
         password: passwordHash, 
     });
-    const savedUser = await newUser.save();
-    res.json(savedUser);        
+    
+    try{
+        const savedUser = await newUser.save();
+        res.json(savedUser); 
+    } catch(error){
+        next(error); // When next is called with an error object, Express recognizes this as an error and will skip any non-error handling middleware. Instead, it will look for error-handling middleware to handle the error.
+    }        
 };
