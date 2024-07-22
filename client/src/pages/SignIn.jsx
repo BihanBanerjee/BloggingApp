56af04985414d07676c031d8145deb07549aeb89
Bihan Banerjee <banerjeebihan456@gmail.com>
2024-07-22 12:31:03 +0530

    add redux toolkit.

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,104 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  // const [errorMessage, setErrorMessage] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector(state => state.user);
 
-function SignIn() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value.trim()})
+  }
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!formData.email || !formData.password) {
+      // return setErrorMessage('Please fill all the fields');
+      return dispatch(signInFailure('Please fill all the fields')); // 👆 doing the same thing as the above line. 
+    }
+    try {
+      // setLoading(true);
+      // setErrorMessage(null);
+      dispatch(signInStart()); // 👆 doing the same thing as the above 2 lines of code. 
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+      });
+      const data = await res.json();
+      if(data.success === false) {
+        // return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message)); // 👆 doing the same thing as the above line.
+      }
+      // setLoading(false);
+      if(res.ok) {
+        dispatch(signInSuccess(data));
+        navigate('/');
+      }
+      // console.log(data);
+    } catch(error) {
+      // setErrorMessage(error.message);
+      // setLoading(false);
+      dispatch(signInFailure(error.message));
+    }
+  }
   return (
-    <div>SignIn</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left */}
+        <div className='flex-1'>
+            <Link to="/" className="font-bold dark:text-white text-4xl">
+              <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-300 to-purple-200 rounded-lg text-gray-800'>
+                100xdev
+              </span>
+              <span className='text-gray-800'>
+                Blogs
+              </span>
+            </Link>
+            <p className='text-sm mt-5'>
+              This is a demo project. You can sign in with your email and password or with Google.
+            </p>
+        </div>
+        {/* right */}
+        <div className='flex-1'>
+            <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+              <div>
+                <Label value='Your Email' />
+                <TextInput type='email' placeholder='name@company.com' id='email' onChange={handleChange} />
+              </div>
+
+              <div>
+                <Label value='Your Password' />
+                <TextInput type='password' placeholder='***************' id='password' onChange={handleChange} />
+              </div>
+              <Button gradientDuoTone="purpleToBlue" type='submit' disabled={loading}>
+                {loading ? (<>
+                  <Spinner size="sm"></Spinner>
+                  <span className='pl-3'>Loading...</span>
+                </>) : 'Sign In'}
+              </Button>
+            </form>
+            <div className='flex gap-2 text-sm mt-4'>
+              <span>Dont have an account?</span>
+              <Link to='/signup' className='text-blue-500 underline'>
+                Sign Up
+              </Link>
+            </div>
+            {errorMessage && (
+              <Alert className='mt-5' color="failure">
+                {errorMessage}
+              </Alert>
+            )}
+        </div>
+      </div>
+    </div>
   )
 }
-
-export default SignIn
