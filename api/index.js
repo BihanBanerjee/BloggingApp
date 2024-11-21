45323026fe041620a75fe292503b2bff5572390f
Bihan Banerjee <banerjeebihan456@gmail.com>
2024-11-21 20:19:56 +0530

    Add comment section to the post page

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import postRoutes from './routes/post.route.js'
+import commentRoutes from './routes/comment.route.js'
 
 dotenv.config();
 
@@ -28,6 +29,7 @@ app.use(cookieParser());
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
 app.use((err, req, res, next) => { // This is a global catch.
     const statusCode = err.statusCode || 500;
